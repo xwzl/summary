318c8f507c2a57c453bef288cd026cae64505ec9
徐伟智 <624244232@qq.com>
2020-09-25 00:21:09 +0800

    AQS 详解

---
+++ turing-java/src/main/java/com/turing/java/jvm/concurrent/pc/NumbersConsumer.java
@@ -0,0 +1,31 @@
+package com.turing.java.jvm.concurrent.pc;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.BlockingQueue;
+
+
+@Slf4j
+public class NumbersConsumer implements Runnable {
+    private BlockingQueue<Integer> queue;
+    private final int poisonPill;
+
+    public NumbersConsumer(BlockingQueue<Integer> queue, int poisonPill) {
+        this.queue = queue;
+        this.poisonPill = poisonPill;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                Integer number = queue.take();
+                if (number.equals(poisonPill)) {
+                    return;
+                }
+                log.info("武大郎-{}号,喝药-编号:{}",Thread.currentThread().getId(),number);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
